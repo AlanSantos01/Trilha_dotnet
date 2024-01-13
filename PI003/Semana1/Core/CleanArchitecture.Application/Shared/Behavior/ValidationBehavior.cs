aa2841d78116f6c9209b5d48ddb656ec351e2929
EzekLobo <eloliveira.cic@uesc.br>
2024-01-13 16:42:55 -0300

    desenvolvimento dos testes e validações

--- PI003/Semana1/Core/CleanArchitecture.Application/Shared/Behavior/ValidationBehavior.cs
+++ PI003/Semana1/Core/CleanArchitecture.Application/Shared/Behavior/ValidationBehavior.cs
@@ -12,23 +12,19 @@ public class ValidationBehavior<TRequest, TResponse>: IPipelineBehavior<TRequest
     }
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken){
 
-        if (!_validators.Any())
-                return await next();
+        if (!_validators.Any())return await next();
+        var context =new ValidationContext<TRequest>(request);
 
         if(!_validators.Any() ){
-            var context =new ValidationContext<TRequest>(request);
+             context =new ValidationContext<TRequest>(request);
 
             var validationResults = await Task.WhenAll(_validators.Select(v=> v.ValidateAsync(context, cancellationToken)));
             
-            var failures = validationResults.SelectMany(r => r.Errors).Where(failures => failures != null).ToList();
+            var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
 
 
              if (failures.Count != 0)
                 throw new FluentValidation.ValidationException(failures);
-
-
-            
-            
 
         }
         return await next();
