20096cb99df05838f40894635132688c9add0aff
EzekLobo <eloliveira.cic@uesc.br>
2024-01-13 11:16:40 -0300

    Resolução de erros

---
+++ PI003/Semana1/Core/CleanArchitecture.Application/Services/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Application.Services;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApplicationApp(this IServiceCollection services){
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg=> cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
--- PI003/Semana1/Presentation/CleanArchtecture.API/Program.cs
+++ PI003/Semana1/Presentation/CleanArchtecture.API/Program.cs
@@ -1,25 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence;
+using CleanArchitecture.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+builder.Services.configurePersistenceApp(builder.Configuration);
+
+builder.Services.ConfigureApplicationApp();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+CreateDatabase(app);
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
+app.Run();
+
+
 
 app.Run();
+
+static void CreateDatabase(WebApplication app){
+    var serviceScope = app.Services.CreateScope();
+    var datContex =serviceScope.ServiceProvider.GetService<AppDbContext>();
+    datContex?.Database.EnsureCreated();
+
+}
